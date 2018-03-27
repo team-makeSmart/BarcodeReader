1a557bc0498bddf40a06c859af92e9cddf5f9f5d
Mcghee-Jake <jmcghee@csumb.edu>
2018-03-27 12:59:12 -0700

    added "public" to constructors, deleted old class files

--- src/BarcodeIO.java
+++
@@ -1,9 +0,0 @@
-interface BarcodeIO
-{
-   public boolean scan(BarcodeImage bc);
-   public boolean readText(String text);
-   public boolean generateImageFromText();
-   public boolean translateImageToText();
-   public void displayTextToConsole();
-   public void displayImageToConsole();
-}
